406cb0cce98bb3cebde2fc24ab25302ccc49b6bf
Enes <enesfplt@gmail.com>
2024-05-29 14:55:31 -0400

    LeetCode

--- RemoveElement.java
+++ RemoveElement.java
@@ -18,3 +18,31 @@ class Solution {
         return newArrayIndex;
     }
 }
+
+//------------------------------------------
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        // Initialize two pointers
+        int left = 0;  // Start from the beginning of the array
+        int right = nums.length - 1;  // Start from the end of the array
+        
+        // Loop until the two pointers cross
+        while (left <= right) {
+            if (nums[left] == val) {
+                // If the element at the left pointer is the target value,
+                // replace it with the element at the right pointer
+                nums[left] = nums[right];
+                // Decrement the right pointer since we have handled that element
+                right--;
+            } else {
+                // If the element at the left pointer is not the target value,
+                // just move the left pointer to the next element
+                left++;
+            }
+        }
+        
+        // Return the new length of the array which is the position of the left pointer
+        return left;
+    }
+}
