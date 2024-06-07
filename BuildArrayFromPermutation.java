ba5cac9512466a6fdc01ef7fd77afdf944cfeb06
Enes <enesfplt@gmail.com>
2024-06-06 23:27:41 -0400

    LeetCode

--- BuildArrayFromPermutation.java
+++ BuildArrayFromPermutation.java
@@ -3,16 +3,16 @@ class Solution {
         // Get the length of the input array
         int n = nums.length;
         
-        // Initialize the result array with the same length as nums
-        int[] result = new int[n];
+        // Initialize the ans array with the same length as nums
+        int[] ans = new int[n];
         
         // Iterate over each index in the nums array
         for (int i = 0; i < n; i++) {
-            // Set result[i] to the value at the index nums[i] in the nums array
-            result[i] = nums[nums[i]];
+            // Set ans[i] to the value at the index nums[i] in the nums array
+            ans[i] = nums[nums[i]];
         }
         
-        // Return the constructed result array
-        return result;
+        // Return the constructed ans array
+        return ans;
     }
 }
